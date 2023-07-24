87b513db98e1f52b1ec45bcfbe8679d43303576d
Ansh <26479077+AnshGupta01@users.noreply.github.com>
2023-07-24 09:52:44 +0530

    Text edit added

--- Spring_rest_automatic/src/main/java/com/data/rest/spring/BookRepo.java
+++ Spring_rest_automatic/src/main/java/com/data/rest/spring/BookRepo.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path = "mybooks", collectionResourceRel = "boo")
+
+
 public interface BookRepo extends JpaRepository<Book, Integer> {
 }
