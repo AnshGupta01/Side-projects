974f65606b5147aeb992567a4c496bf80211a824
Ansh <26479077+AnshGupta01@users.noreply.github.com>
2023-07-27 17:01:34 +0530

    Updated The repo
    Repo Cleanup

--- Spring_core_practice/src/main/java/com/springcore/autowire/annotation/test.java
+++ Spring_core_practice/src/main/java/com/springcore/autowire/annotation/test.java
@@ -1,7 +1,11 @@
 package com.springcore.autowire.annotation;
 
+import org.springcore.ref.A;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class test {
     public static void main(String[] args) {
