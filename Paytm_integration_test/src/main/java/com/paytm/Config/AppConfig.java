2993da62df63b3550c4c0868a2547af65c7656c0
Ansh <26479077+AnshGupta01@users.noreply.github.com>
2023-07-28 15:41:12 +0530

    Saved state: Awaiting Paytm API KEY

---
+++ Paytm_integration_test/src/main/java/com/paytm/Config/AppConfig.java
@@ -0,0 +1,7 @@
+package com.paytm.Config;
+
+public class AppConfig {
+    public static final String MID="RHLBnF07039627707073";
+    public static final String MKEY="BXWmXOntYo858BTG";
+    public static final String website = "WEBSTAGING";
+}
