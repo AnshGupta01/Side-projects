b85afd293bb2e26bb8167e4264c31608dbaf2f1e
Ansh <26479077+AnshGupta01@users.noreply.github.com>
2023-09-25 19:28:14 +0530

    Updation

--- Front-end Projects/react-redux-todos/src/redux/actions/action-types.js
+++
@@ -1,6 +0,0 @@
-//Todo action types
-export const ADD_TODO = "ADD_TODO";
-export const DELETE_TODO = "DELETE_TODO";
-export const UPDATE_TODO = "UPDATE_TODO";
-
-//user action types
--- Front-end Projects/react-redux-todos/src/redux/store/store.js
+++
@@ -1,7 +0,0 @@
-import todoReducer from "../reducers/todoReducer";
-import { createStore, combineReducers } from "redux";
-
-const rootReducer = combineReducers({ todo: todoReducer });
-const store = createStore(rootReducer);
-
-export default store;
